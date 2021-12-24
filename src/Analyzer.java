1374e51520fe2e499bd32493b2d5daceb4d61ff9
Bhavyai Gupta <zbhavyai@gmail.com>
2021-12-24 15:19:01 -0700

    identation tabs to spaces

--- src/Analyzer.java
+++ src/Analyzer.java
@@ -13,168 +13,168 @@ import java.io.Console;
  * @since 1.0
  */
 public class Analyzer {
-	private Console console;
-	private String COLOR;
-	private String YELOW;
-	private String RESET;
-	/**
-	 * <code>true</code> if there has been atleast one flag raised by the program;
-	 * <code>false</code> otherwise
-	 */
-	private boolean atleastOne;
+    private Console console;
+    private String COLOR;
+    private String YELOW;
+    private String RESET;
+    /**
+     * <code>true</code> if there has been atleast one flag raised by the program;
+     * <code>false</code> otherwise
+     */
+    private boolean atleastOne;
 
-	/**
-	 * Constructs a Analyzer object with default values of the data fields.
-	 */
-	Analyzer() {
-		console = System.console();
-		COLOR = "\033[0;91m";
-		YELOW = "\033[0;93m";
-		RESET = "\033[0m";
-		atleastOne = false;
-	}
+    /**
+     * Constructs a Analyzer object with default values of the data fields.
+     */
+    Analyzer() {
+        console = System.console();
+        COLOR = "\033[0;91m";
+        YELOW = "\033[0;93m";
+        RESET = "\033[0m";
+        atleastOne = false;
+    }
 
-	/**
-	 * Loop over all the lines of the python file, and pass each line to
-	 * lineAnalyzer(). Print start and end messages.
-	 *
-	 * @param pyFile The <code>File</code> pointing to the .py file
-	 */
-	public void fileAnalyzer(File pyFile) {
-		int lineCount = 0;
-		try {
-			Scanner sc = new Scanner(new BufferedReader(new FileReader(pyFile)));
+    /**
+     * Loop over all the lines of the python file, and pass each line to
+     * lineAnalyzer(). Print start and end messages.
+     *
+     * @param pyFile The <code>File</code> pointing to the .py file
+     */
+    public void fileAnalyzer(File pyFile) {
+        int lineCount = 0;
+        try {
+            Scanner sc = new Scanner(new BufferedReader(new FileReader(pyFile)));
 
-			console.printf("%n%nChecking file %s%s%s%n", YELOW, pyFile.getName(), RESET);
+            console.printf("%n%nChecking file %s%s%s%n", YELOW, pyFile.getName(), RESET);
 
-			while (sc.hasNextLine()) {
-				this.lineAnalyzer(sc.nextLine(), ++lineCount);
-			}
+            while (sc.hasNextLine()) {
+                this.lineAnalyzer(sc.nextLine(), ++lineCount);
+            }
 
-			if (atleastOne == false) {
-				console.printf("%n%nConformed!%n%n");
-			}
+            if (atleastOne == false) {
+                console.printf("%n%nConformed!%n%n");
+            }
 
-			else {
-				console.printf("%n%nChecks complete!%n%n");
-			}
+            else {
+                console.printf("%n%nChecks complete!%n%n");
+            }
 
-			sc.close();
-		}
+            sc.close();
+        }
 
-		catch (FileNotFoundException e) {
-			console.printf("%n%nFile not found! Exit%n");
-			System.exit(1);
-		}
-	}
+        catch (FileNotFoundException e) {
+            console.printf("%n%nFile not found! Exit%n");
+            System.exit(1);
+        }
+    }
 
-	/**
-	 * Loop over all the characters of the String, to analyze the PEP8 conformity
-	 *
-	 * @param str       The string passed to be analyzed by the function
-	 * @param lineCount The current line number in the file
-	 */
-	private void lineAnalyzer(String str, int lineCount) {
-		// dont access empty lines
-		if (str.length() == 0) {
-			return;
-		}
+    /**
+     * Loop over all the characters of the String, to analyze the PEP8 conformity
+     *
+     * @param str       The string passed to be analyzed by the function
+     * @param lineCount The current line number in the file
+     */
+    private void lineAnalyzer(String str, int lineCount) {
+        // dont access empty lines
+        if (str.length() == 0) {
+            return;
+        }
 
-		char prevChar = '\n';
-		char currChar;
-		char nextChar = '\n';
-		boolean commentMode = false;
-		boolean stringMode = false;
+        char prevChar = '\n';
+        char currChar;
+        char nextChar = '\n';
+        boolean commentMode = false;
+        boolean stringMode = false;
 
-		// loop through all the characters of the string
-		for (int i = 0; i < str.length(); i++) {
-			currChar = str.charAt(i);
+        // loop through all the characters of the string
+        for (int i = 0; i < str.length(); i++) {
+            currChar = str.charAt(i);
 
-			// read the nextChar ahead, to make easier ahead
-			// if nextChar can't be read, its already a '\n'
-			if ((i + 1) < str.length()) {
-				nextChar = str.charAt(i + 1);
-			}
+            // read the nextChar ahead, to make easier ahead
+            // if nextChar can't be read, its already a '\n'
+            if ((i + 1) < str.length()) {
+                nextChar = str.charAt(i + 1);
+            }
 
-			// if encountered pound sign, turn on the commentMode
-			// to prevent style warnings in comments
-			if (currChar == '#') {
-				commentMode = true;
-			}
+            // if encountered pound sign, turn on the commentMode
+            // to prevent style warnings in comments
+            if (currChar == '#') {
+                commentMode = true;
+            }
 
-			// if encounterd quote sign, toggle the stringMode
-			// to prevent style warnings in strings
-			if ((currChar == '\"') && (stringMode == false)) {
-				stringMode = true;
-			}
+            // if encounterd quote sign, toggle the stringMode
+            // to prevent style warnings in strings
+            if ((currChar == '\"') && (stringMode == false)) {
+                stringMode = true;
+            }
 
-			else if (currChar == '\"' && (stringMode == true)) {
-				stringMode = false;
-			}
+            else if (currChar == '\"' && (stringMode == true)) {
+                stringMode = false;
+            }
 
-			// RULE - no tab indentation (no tabs anywhere)
-			if (currChar == '\t') {
-				console.printf("%n%sLine %4d%s: %s %d", COLOR, lineCount, RESET, "tab found at column ", i + 1);
-				this.atleastOne = true;
-			}
+            // RULE - no tab indentation (no tabs anywhere)
+            if (currChar == '\t') {
+                console.printf("%n%sLine %4d%s: %s %d", COLOR, lineCount, RESET, "tab found at column ", i + 1);
+                this.atleastOne = true;
+            }
 
-			// RULE - space after comment starts
-			else if ((currChar == '#') && !((nextChar == ' ') || (nextChar == '\n'))) {
-				console.printf("%n%sLine %4d%s: %s %d", COLOR, lineCount, RESET, "no space after # at column", i + 1);
-				this.atleastOne = true;
-			}
+            // RULE - space after comment starts
+            else if ((currChar == '#') && !((nextChar == ' ') || (nextChar == '\n'))) {
+                console.printf("%n%sLine %4d%s: %s %d", COLOR, lineCount, RESET, "no space after # at column", i + 1);
+                this.atleastOne = true;
+            }
 
-			// RULE - space around every operator
-			else if (isOperator(currChar)) {
-				if (!(isOperator(prevChar) || prevChar == ' ')) {
-					if (!commentMode && !stringMode) {
-						console.printf("%n%sLine %4d%s: %s %c %s %d", COLOR, lineCount, RESET, "no space before",
-								currChar, "at column", i + 1);
-						this.atleastOne = true;
-					}
-				}
+            // RULE - space around every operator
+            else if (isOperator(currChar)) {
+                if (!(isOperator(prevChar) || prevChar == ' ')) {
+                    if (!commentMode && !stringMode) {
+                        console.printf("%n%sLine %4d%s: %s %c %s %d", COLOR, lineCount, RESET, "no space before",
+                                currChar, "at column", i + 1);
+                        this.atleastOne = true;
+                    }
+                }
 
-				if (!(isOperator(nextChar) || nextChar == ' ')) {
-					if (!commentMode && !stringMode) {
-						console.printf("%n%sLine %4d%s: %s %c %s %d", COLOR, lineCount, RESET, "no space after",
-								currChar, "at column", i + 1);
-						this.atleastOne = true;
-					}
-				}
-			}
+                if (!(isOperator(nextChar) || nextChar == ' ')) {
+                    if (!commentMode && !stringMode) {
+                        console.printf("%n%sLine %4d%s: %s %c %s %d", COLOR, lineCount, RESET, "no space after",
+                                currChar, "at column", i + 1);
+                        this.atleastOne = true;
+                    }
+                }
+            }
 
-			prevChar = currChar;
-		}
-	}
+            prevChar = currChar;
+        }
+    }
 
-	/**
-	 * Check if the parameter passed is an operator in python
-	 *
-	 * @param c of type <code>char</code>
-	 * @return <code>true</code> if c is of type <code>char</code>;
-	 *         <code>false</code> otherwise
-	 */
-	private boolean isOperator(char c) {
-		boolean flag = false;
+    /**
+     * Check if the parameter passed is an operator in python
+     *
+     * @param c of type <code>char</code>
+     * @return <code>true</code> if c is of type <code>char</code>;
+     *         <code>false</code> otherwise
+     */
+    private boolean isOperator(char c) {
+        boolean flag = false;
 
-		switch (c) {
-			case '=':
-			case '+':
-			case '-':
-			case '*':
-			case '/':
-			case '%':
-			case '!':
-			case '>':
-			case '<':
-			case '&':
-			case '|':
-			case '^':
-			case '~':
-				flag = true;
-				break;
-		}
+        switch (c) {
+            case '=':
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+            case '%':
+            case '!':
+            case '>':
+            case '<':
+            case '&':
+            case '|':
+            case '^':
+            case '~':
+                flag = true;
+                break;
+        }
 
-		return flag;
-	}
+        return flag;
+    }
 }
--- src/App.java
+++ src/App.java
@@ -8,71 +8,71 @@ import java.io.File;
  * @since 1.0
  */
 public class App {
-	/**
-	 * The file created based on user input would be stored here.
-	 */
-	private File pythonFile;
-	private Console console;
+    /**
+     * The file created based on user input would be stored here.
+     */
+    private File pythonFile;
+    private Console console;
 
-	/**
-	 * Constructs a App object and initializes the console.
-	 */
-	public App() {
-		this.console = System.console();
+    /**
+     * Constructs a App object and initializes the console.
+     */
+    public App() {
+        this.console = System.console();
 
-		if (this.console == null) {
-			System.err.println("%n%nApplication not started in console");
-			Runtime.getRuntime().exit(1);
-		}
-	}
+        if (this.console == null) {
+            System.err.println("%n%nApplication not started in console");
+            Runtime.getRuntime().exit(1);
+        }
+    }
 
-	/**
-	 * Get a string representing a valid python file from the user.
-	 */
-	void getValidString() {
-		String temp = null;
+    /**
+     * Get a string representing a valid python file from the user.
+     */
+    void getValidString() {
+        String temp = null;
 
-		while (true) {
-			temp = this.console.readLine("%n%nEnter the full path of the python filename: ");
+        while (true) {
+            temp = this.console.readLine("%n%nEnter the full path of the python filename: ");
 
-			if (isValidFile(temp)) {
-				return;
-			}
+            if (isValidFile(temp)) {
+                return;
+            }
 
-			this.console.printf("%n%nPlease enter a valid python file name with .py extension");
-		}
-	}
+            this.console.printf("%n%nPlease enter a valid python file name with .py extension");
+        }
+    }
 
-	/**
-	 * Check if the string passed to this function is a valid python file.
-	 *
-	 * @param pythonFilePath
-	 * @return {@code true} is file is valid python file, {@code false} otherwise
-	 */
-	boolean isValidFile(String pythonFilePath) {
-		this.pythonFile = new File(pythonFilePath);
+    /**
+     * Check if the string passed to this function is a valid python file.
+     *
+     * @param pythonFilePath
+     * @return {@code true} is file is valid python file, {@code false} otherwise
+     */
+    boolean isValidFile(String pythonFilePath) {
+        this.pythonFile = new File(pythonFilePath);
 
-		if (pythonFile.isFile() && pythonFile.getAbsolutePath().endsWith(".py")) {
-			return true;
-		}
+        if (pythonFile.isFile() && pythonFile.getAbsolutePath().endsWith(".py")) {
+            return true;
+        }
 
-		else {
-			return false;
-		}
-	}
+        else {
+            return false;
+        }
+    }
 
-	public static void main(String[] args) {
-		App a = new App();
+    public static void main(String[] args) {
+        App a = new App();
 
-		if (args.length == 0) {
-			a.getValidString();
-		}
+        if (args.length == 0) {
+            a.getValidString();
+        }
 
-		else if (!a.isValidFile(args[0])) {
-			a.getValidString();
-		}
+        else if (!a.isValidFile(args[0])) {
+            a.getValidString();
+        }
 
-		Analyzer an = new Analyzer();
-		an.fileAnalyzer(a.pythonFile);
-	}
+        Analyzer an = new Analyzer();
+        an.fileAnalyzer(a.pythonFile);
+    }
 }
